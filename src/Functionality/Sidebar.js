5e13989ec88b919dda2e022096760d4b9ff96e16
deucaleon18 <shubham37908@gmail.com>
2021-05-28 20:53:35 +0530

    login route

--- src/Functionality/Sidebar.js
+++ src/Functionality/Sidebar.js
@@ -8,13 +8,13 @@ import "../styles/sidebar.css"
          
         <div className="sidebar">
         <ul>
-            <li><a href="http://localhost:3000/home">Home</a></li>
-            <li><a href="http://localhost:3000/search">Search</a></li>
-            <li><a href="http://localhost:3000/library">Your Library</a></li>
-            <li><a href="http://localhost:3000/login" onClick={()=>{localStorage.removeItem('token')}}>Logout</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/search">Search</a></li>
+            <li><a href="/library">Your Library</a></li>
+            <li><a href="/login" onClick={()=>{localStorage.removeItem('token')}}>Logout</a></li>
             <h4>PLAYLISTS</h4>
-            <li><a href="http://localhost:3000/create-playlist">Create Playlist</a></li>
-            <li><a href="http://localhost:3000/liked">Liked Songs</a></li>
+            <li><a href="/create-playlist">Create Playlist</a></li>
+            <li><a href="liked">Liked Songs</a></li>
             {/* <h6><a href="#">Inside your Head</a></h6>
             <h6><a href="#">My Playlist #1</a></h6> */}
         </ul>
