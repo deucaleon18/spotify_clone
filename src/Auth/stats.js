5e2c36ad6fc303ba89f1d7e6b883c3a8b61ca268
deucaleon18 <shubham37908@gmail.com>
2021-06-06 17:18:12 +0530

    demo build

--- src/Auth/stats.js
+++ src/Auth/stats.js
@@ -5,5 +5,4 @@ export const perms=[
 export const url="https://deezerclone.herokuapp.com/https://api.deezer.com/"
 export const callbackURL="https://spotify-clone-teal.vercel.app/loading";
 export const loginURL=`https://connect.deezer.com/oauth/auth.php?app_id=${app_id}&redirect_uri=${callbackURL}&perms=${perms}`;
-export const md5 = require('md5');
 export const secret='c07d137e2a0dfc3d1fa0a3a11a3f9148';
