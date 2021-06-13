9e26f75b59911e86a655053787513d339f6794e5
deucaleon18 <shubham37908@gmail.com>
2021-06-14 01:21:28 +0530

    changing the UI

--- src/Auth/stats.js
+++ src/Auth/stats.js
@@ -3,6 +3,6 @@ export const perms=[
     "basic_access","email","offline_access","manage_library","manage_community","delete_library","listening_history"
 ];
 export const url="https://deezerclone.herokuapp.com/https://api.deezer.com/"
-export const callbackURL="https://spotify-clone-teal.vercel.app/loading";
+export const callbackURL="http://localhost:3000/loading";
 export const loginURL=`https://connect.deezer.com/oauth/auth.php?app_id=${app_id}&redirect_uri=${callbackURL}&perms=${perms}`;
 export const secret='c07d137e2a0dfc3d1fa0a3a11a3f9148';
